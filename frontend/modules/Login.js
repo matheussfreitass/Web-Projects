997e040969006377f8946e018e48829e0b15b536
Matheus Freitas <mffreitas17@gmail.com>
2024-12-12 15:11:21 -0300

    Normalizando os problemas nas linhas

---
+++ frontend/modules/Login.js
@@ -0,0 +1,19 @@
+import validator from 'validator';
+
+export default class Login {
+  constructor(formClass) {
+    this.form = document.querySelector(formClass);
+  }
+
+  init() {
+    this.events();
+  }
+
+  events() {
+    if(!this.form) return;
+    this.form.addEventListener('submit', e => {
+      e.preventDefault();
+      this.validate(e);
+    });
+  }
+}
